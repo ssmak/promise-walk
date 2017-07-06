68b47988059ae34737d2b4b5433306c3c7889ee4
ssmak <steave.mkm@hotmail.com>
2017-07-06 16:01:48 +0800

    Period commit.

--- index.js
+++ index.js
@@ -1,23 +1,27 @@
 const PromiseWalk = {
+	results: [],
 	index: 0,
 	walk: function (promises, callback) {
 		(function (promiseWalk, promises, callback) {
 			if(promiseWalk.index < promises.length) {
 				promises[promiseWalk.index]()
-				.then(function () {
+				.then(function (result) {
+					if(result) {
+						promiseWalk.results.push(result);
+					}
 					promiseWalk.index++;
 					promiseWalk.walk(promises, callback);
 				})
 				.catch(function () {
-					callback();
+					promiseWalk.index++;
+					promiseWalk.walk(promises, callback);
+					//callback(promiseWalk.results);
 				});
 			} else {
-				callback();
+				callback(promiseWalk.results);
 			}
 		})(this, promises, callback);
 	}
 }
 
 module.exports = PromiseWalk;
-
-
